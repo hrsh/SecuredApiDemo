2461f3229e9414deb4ead00d98adf6df5a777855
hrshojaie <hrshojaie@gmail.com>
2021-03-14 13:34:52 +0330

    add product controller

---
+++ src/ProductApi/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductApi.Data;
+using ProductApi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ProductApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> List()
+        {
+            var t = await _context
+                .Products
+                .ToListAsync();
+            return t;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Product> Get(int id)
+            => await _context.Products.FirstOrDefaultAsync(a => a.Id == id);
+    }
+}
