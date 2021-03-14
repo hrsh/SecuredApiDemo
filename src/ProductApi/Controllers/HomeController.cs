a2a742701062085a76186544439db53888a2c24d
hrshojaie <hrshojaie@gmail.com>
2021-03-14 13:25:42 +0330

    add init migrations

---
+++ src/ProductApi/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Index() => Ok("Product Api");
+    }
+}
---
+++ src/ProductApi/Migrations/20210314095523_Init.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProductApi.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Price", "Title" },
+                values: new object[] { 1, 1000, "Product 1" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Price", "Title" },
+                values: new object[] { 2, 2000, "Product 2" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Price", "Title" },
+                values: new object[] { 3, 3000, "Product 3" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
