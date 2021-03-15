420a34fa4b945c4095753c576b6373ac3b999194
hrshojaie <hrshojaie@gmail.com>
2021-03-15 10:55:32 +0330

    add identity server 4

---
+++ Is4/Config.cs
@@ -0,0 +1,14 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Is4
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+
+            };
+    }
+}
