974b668705ba3a081b424ef4fa75a11cc09db14e
hrshojaie <hrshojaie@gmail.com>
2021-03-15 11:05:51 +0330

    config is4

--- Is4/Config.cs
+++ Is4/Config.cs
@@ -8,7 +8,22 @@ namespace Is4
         public static IEnumerable<ApiScope> ApiScopes =>
             new List<ApiScope>
             {
+                new ApiScope("api1", "Product Api")
+            };
 
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "api1" }
+                }
             };
     }
 }
---
+++ Is4/Controllers/DefaultController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Is4.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class DefaultController : ControllerBase
+    {
+        public DefaultController()
+        {
+
+        }
+
+        public IActionResult Index() => Ok("IdentityServer4.Api");
+    }
+}
